4b012baeaf5eb0284fadc8de6fdc5f5616baaa6e
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-09-29 01:57:30 +0200

    Add Parse SDK
    - Add RibbitApplication class as entry point for the app and for basic configuration
    - Follow these steps to import the Parse jar file *as a module*: https://www.parse.com/questions/importing-library-in-android-studio
    - It is save to put the application keys under version cotrol: http://blog.parse.com/2014/06/30/parse-security-i-are-you-the-key-master/

---
+++ app/src/main/java/de/sebastianroeder/ribbit/RibbitApplication.java
@@ -0,0 +1,19 @@
+package de.sebastianroeder.ribbit;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "Dk8PBxqXVncK0XogTB8Fhi2bdjvfQisi87u8W9Pw",
+                "qAFCCTQOdTSn5PoHKBRJXuGAeYaLF05NPvXpDna8");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
