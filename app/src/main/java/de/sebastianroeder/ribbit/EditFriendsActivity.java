390857a5969d0607325faaa7e9ff721fe5a27b36
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-10-04 21:18:59 +0200

    Store friends relation in backend

--- app/src/main/java/de/sebastianroeder/ribbit/EditFriendsActivity.java
+++ app/src/main/java/de/sebastianroeder/ribbit/EditFriendsActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,17 +25,26 @@ import java.util.List;
 public class EditFriendsActivity extends ListActivity {
 
     public static final String TAG = EditFriendsActivity.class.getSimpleName();
+    protected ParseUser mCurrentUser;
+    protected ParseRelation<ParseUser> mFriendRelation;
+    protected List<ParseUser> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
         setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
         userQuery.orderByAscending(ParseConstants.KEY_USERNAME);
@@ -41,7 +54,7 @@ public class EditFriendsActivity extends ListActivity {
             public void done(List<ParseUser> parseUsers, ParseException e) {
                 setProgressBarIndeterminateVisibility(false);
                 if (e == null) {
-                    List<ParseUser> mUsers = parseUsers;
+                    mUsers = parseUsers;
                     List<String> userNames = new ArrayList<String>(mUsers.size());
                     // TODO: do not include currently logged in user in list
                     for (ParseUser user : mUsers) {
@@ -80,5 +93,26 @@ public class EditFriendsActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseUser clickedUser = mUsers.get(position);
+        if (l.isItemChecked(position)) {
+            mFriendRelation.add(clickedUser);
+        } else {
+            mFriendRelation.remove(clickedUser);
+        }
+
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/de/sebastianroeder/ribbit/ParseConstants.java
+++ app/src/main/java/de/sebastianroeder/ribbit/ParseConstants.java
@@ -2,4 +2,5 @@ package de.sebastianroeder.ribbit;
 
 public final class ParseConstants {
     public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 }
